510315f82dca5ea2f4ae74c97c1c1e0421068d5b
Marcin Grabowiecki <m.grabowiecki4@gmail.com>
2021-09-22 11:34:54 +0200

    Add show lesson view

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -1,8 +1,17 @@
 class LessonController < ApplicationController
+  before_action :set_lesson, only: [:show]
+
   def index
     @lessons = Lesson.all
   end
 
   def show
+    @lesson
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
   end
 end
